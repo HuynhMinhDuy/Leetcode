cfa3e3c3be1ee3ddcf85691f6bbd0bed8d5e513a
HuynhMinhDuy <79966404+HuynhMinhDuy@users.noreply.github.com>
2023-04-22 09:58:23 +0700

    Update Longest_Substring_Without_Repeating_Characters.cpp

--- Longest_Substring_Without_Repeating_Characters.cpp
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,3 +1,9 @@
+
+/*
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Given a string s, find the length of the longest substring without repeating characters.
+*/
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
