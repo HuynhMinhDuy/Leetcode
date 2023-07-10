86499557af8c3ae2115284f90a4a1dfea832ab8c
hmd <79966404+hmdresearch@users.noreply.github.com>
2023-07-10 16:03:01 +0700

    Update 307_Range_Sum_Query_Mutable.java

--- 307_Range_Sum_Query_Mutable.java
+++ 307_Range_Sum_Query_Mutable.java
@@ -12,48 +12,46 @@ int sumRange(int left, int right) Returns the sum of the elements of nums betwee
 */
 
 class NumArray {
+
+    private int[] nums;
     private int[] BIT;
-    private int N;
-    private int[] arr;
 
     public NumArray(int[] nums) {
-        N = nums.length + 1;
-        BIT = new int[N];
-        arr = new int[N - 1];
-        creatBIT(nums);       
-    }
-
-    public void creatBIT(int[] nums) {
-        for(int index = 0; index < nums.length; ++index) {
-            update(index, nums[index]);
-        }
+        this.nums = new int[nums.length];
+        BIT = new int[nums.length + 1];
+        createBIT(nums);
     }
     
+    public void createBIT(int[] nums) {
+        for(int idx = 0; idx < nums.length; ++idx) {
+            update(idx, nums[idx]);
+        }
+    }
+
     public void update(int index, int val) {
-        int diff = val - arr[index];
-        arr[index] = val;
+        int diff = val - this.nums[index];
+        this.nums[index] = val;
         int idx = index + 1;
-        while(idx < N) {
+        while(idx < BIT.length) {
             BIT[idx] += diff;
             idx += idx & (-idx);
         }
     }
     
     public int sumRange(int left, int right) {
-        int sumRight = 0;
         int sumLeft = 0;
+        int sumRight = 0;
         int l = left;
         int r = right + 1;
+
+        while(l > 0) {
+            sumLeft += BIT[l];
+            l -= l & (-l);
+        }
         while(r > 0) {
             sumRight += BIT[r];
             r -= r & (-r);
         }
-
-        while (l > 0) {
-            sumLeft += BIT[l];
-            l -= l & (-l);
-        }
-
         return sumRight - sumLeft;
     }
 }
