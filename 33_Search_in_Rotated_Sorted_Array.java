44f5b2cde7909b712be61bb32835ee0cdad8b1f8
hmd <79966404+hmdresearch@users.noreply.github.com>
2023-08-10 08:47:26 +0700

    Update 33_Search_in_Rotated_Sorted_Array.java

--- 33_Search_in_Rotated_Sorted_Array.java
+++ 33_Search_in_Rotated_Sorted_Array.java
@@ -11,32 +11,29 @@ You must write an algorithm with O(log n) runtime complexity.
 
 class Solution {
     public int search(int[] nums, int target) {
-      int left = 0;
-      int right = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-      while(left <= right) {
-          int mid = left + (right - left) / 2;
-          if(nums[mid] == target) {
-              return mid;
-          }  else {
-              if(nums[left] <= nums[mid]) {
-                  // Left size
-                    if(nums[left] <= target && target < nums[mid]) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }                  
-              } else if (nums[mid] <= nums[right]) {
-                  // Right size
-                   if(nums[mid] < target && target <= nums[right]) {
-                        left = mid + 1;
-                   } else {
-                       right = mid - 1;
-                   }
-              }
-          }
-      }  
-
-      return -1;   
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if (nums[left] <= nums[mid]) {
+                // Left side
+                if(nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left  = mid + 1;
+                }
+            } else {
+                // Right side
+                if(nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 }
