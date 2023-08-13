441890ffccaf3f4edb7f968ba2c1313fb8feb816
hmd <79966404+hmdresearch@users.noreply.github.com>
2023-08-13 21:41:10 +0700

    Create 2680_Maximum_OR.java

---
+++ 2680_Maximum_OR.java
@@ -0,0 +1,30 @@
+/*
+You are given a 0-indexed integer array nums of length n and an integer k. In an operation, you can choose an element and multiply it by 2.
+
+Return the maximum possible value of nums[0] | nums[1] | ... | nums[n - 1] that can be obtained after applying the operation on nums at most k times.
+
+Note that a | b denotes the bitwise or between two integers a and b.
+
+*/
+
+class Solution {
+    public long maximumOr(int[] nums, int k) {
+        long[] prefix = new long[nums.length + 1];
+        long[] suffix = new long[nums.length + 1];        
+        long ans = 0;
+        for(int i = 0; i < nums.length; ++i) {
+            prefix[i + 1] = prefix[i] | nums[i];
+        }
+
+        for(int i = nums.length - 1; i >=0; --i) {
+            suffix[i] = suffix[i + 1] | nums[i];
+        }
+
+        for(int i = 0; i < nums.length; ++i){
+            long num = (long)nums[i];
+            ans = Math.max(ans, num << k | prefix[i] | suffix[i + 1]);
+        }
+        
+        return ans;
+    }
+}
