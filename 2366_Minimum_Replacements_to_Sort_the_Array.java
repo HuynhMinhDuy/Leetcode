419c5cc4b449427f17dc36bf8d6a340f0df935ad
hmd <79966404+hmdresearch@users.noreply.github.com>
2023-08-30 10:36:20 +0700

    Create 2366_Minimum_Replacements_to_Sort_the_Array.java

---
+++ 2366_Minimum_Replacements_to_Sort_the_Array.java
@@ -0,0 +1,25 @@
+
+/*
+You are given a 0-indexed integer array nums. In one operation you can replace any element of the array with any two elements that sum to it.
+
+For example, consider nums = [5,6,7]. In one operation, we can replace nums[1] with 2 and 4 and convert nums to [5,2,4,7].
+Return the minimum number of operations to make an array that is sorted in non-decreasing order.
+*/
+class Solution {
+    public long minimumReplacement(int[] nums) {
+        long ans = 0;
+        int currentMin = nums[nums.length - 1];
+
+        for(int i = nums.length - 2; i >= 0; --i){
+            if (nums[i] % currentMin == 0) {
+                ans += nums[i] / currentMin - 1;
+            } else {
+                int k = nums[i] / currentMin + 1;
+                ans += k - 1;
+                currentMin = nums[i] / k;
+            }
+        }
+
+        return ans;
+    }
+}
